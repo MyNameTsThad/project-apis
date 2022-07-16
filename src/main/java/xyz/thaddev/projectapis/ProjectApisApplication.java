1079c8a124e5e3469169e60a2b9601a275606c29
MyNameTsThad <pvpthadgaming@gmail.com>
2022-07-16 17:17:34 +0700

    Coding stats endpoints

--- src/main/java/xyz/thaddev/projectapis/ProjectApisApplication.java
+++ src/main/java/xyz/thaddev/projectapis/ProjectApisApplication.java
@@ -9,87 +9,110 @@ import org.springframework.context.event.EventListener;
 import xyz.thaddev.projectapis.chatthreadsystem.ThreadAPIController;
 import xyz.thaddev.projectapis.computercontrolsystem.CommandAPIController;
 import xyz.thaddev.projectapis.computercontrolsystem.StatusResponseManager;
+import xyz.thaddev.projectapis.statssystem.CodingStatsAPIController;
+import xyz.thaddev.projectapis.statssystem.CodingStatsManager;
 import xyz.thaddev.projectapis.timersystem.TimerAPIController;
 import xyz.thaddev.projectapis.timersystem.TimerInstanceAPIController;
 
-import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 @SpringBootApplication
 public class ProjectApisApplication {
-	public static final String authPassword = "<Password>";
-	public static final String computerControlAuthPassword = "<Password>";
-	public static ProjectApisApplication instance;
+    public static final String authPassword = "<Password>";
+    public static final String computerControlAuthPassword = "<Password>";
+    public static final String githubAuth = "<AuthToken>";
+    public static ProjectApisApplication instance;
 
-	public Logger logger = LoggerFactory.getLogger(ProjectApisApplication.class);
+    public Logger logger = LoggerFactory.getLogger(ProjectApisApplication.class);
 
-	private TimerAPIController timerAPIController;
-	private TimerInstanceAPIController timerInstanceAPIController;
-	private CommandAPIController commandAPIController;
-	private ThreadAPIController threadAPIController;
-	private StatusResponseManager statusResponseManager;
+    private TimerAPIController timerAPIController;
+    private TimerInstanceAPIController timerInstanceAPIController;
+    private CommandAPIController commandAPIController;
+    private ThreadAPIController threadAPIController;
+    private StatusResponseManager statusResponseManager;
+    private CodingStatsAPIController codingStatsAPIController;
+    private CodingStatsManager codingStatsManager;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApisApplication.class, args);
+    }
 
-	public ProjectApisApplication() throws IOException {
-		instance = this;
-		new StatusResponseManager();
-	}
+    public ProjectApisApplication() {
+        instance = this;
+        new StatusResponseManager();
+        new CodingStatsManager();
+        getCodingStatsManager().setHackerrankResponse(3, 5, 225);
+    }
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void startUp() {
-		logger.info("Starting TickAll...");
-		new Timer("TickAll").scheduleAtFixedRate(new TimerTask() {
-			int times = 0;
-			@Override
-			public void run() {
-				times++;
-				timerInstanceAPIController.tickAll(times == 10);
-				if (times == 10) times = 0;
-			}
-		}, 1000, 1000);
-	}
+    @EventListener(ApplicationReadyEvent.class)
+    public void startUp() {
+        logger.info("Starting TickAll...");
+        new Timer("TickAll").scheduleAtFixedRate(new TimerTask() {
+            int times = 0;
 
-	public TimerAPIController getTimerAPIController() {
-		return timerAPIController;
-	}
+            @Override
+            public void run() {
+                times++;
+                timerInstanceAPIController.tickAll(times == 10);
+                if (times == 10) times = 0;
+            }
+        }, 1000, 1000);
+    }
 
-	public void setTimerAPIController(TimerAPIController timerAPIController) {
-		this.timerAPIController = timerAPIController;
-	}
+    public TimerAPIController getTimerAPIController() {
+        return timerAPIController;
+    }
 
-	public TimerInstanceAPIController getTimerInstanceAPIController() {
-		return timerInstanceAPIController;
-	}
+    public void setTimerAPIController(TimerAPIController timerAPIController) {
+        this.timerAPIController = timerAPIController;
+    }
 
-	public void setTimerInstanceAPIController(TimerInstanceAPIController timerInstanceAPIController) {
-		this.timerInstanceAPIController = timerInstanceAPIController;
-	}
+    public TimerInstanceAPIController getTimerInstanceAPIController() {
+        return timerInstanceAPIController;
+    }
 
-	public CommandAPIController getCommandAPIController() {
-		return commandAPIController;
-	}
+    public void setTimerInstanceAPIController(TimerInstanceAPIController timerInstanceAPIController) {
+        this.timerInstanceAPIController = timerInstanceAPIController;
+    }
 
-	public void setCommandAPIController(CommandAPIController commandAPIController) {
-		this.commandAPIController = commandAPIController;
-	}
+    public CommandAPIController getCommandAPIController() {
+        return commandAPIController;
+    }
 
-	public ThreadAPIController getThreadAPIController() {
-		return threadAPIController;
-	}
+    public void setCommandAPIController(CommandAPIController commandAPIController) {
+        this.commandAPIController = commandAPIController;
+    }
 
-	public void setThreadAPIController(ThreadAPIController threadAPIController) {
-		this.threadAPIController = threadAPIController;
-	}
+    public ThreadAPIController getThreadAPIController() {
+        return threadAPIController;
+    }
 
-	public StatusResponseManager getStatusResponseManager() {
-		return statusResponseManager;
-	}
+    public void setThreadAPIController(ThreadAPIController threadAPIController) {
+        this.threadAPIController = threadAPIController;
+    }
 
-	public void setStatusResponseManager(StatusResponseManager statusResponseManager) {
-		this.statusResponseManager = statusResponseManager;
-	}
+    public StatusResponseManager getStatusResponseManager() {
+        return statusResponseManager;
+    }
+
+    public void setStatusResponseManager(StatusResponseManager statusResponseManager) {
+        this.statusResponseManager = statusResponseManager;
+    }
+
+    public CodingStatsAPIController getCodingStatsAPIController() {
+        return codingStatsAPIController;
+    }
+
+    public void setCodingStatsAPIController(CodingStatsAPIController codingStatsAPIController) {
+        this.codingStatsAPIController = codingStatsAPIController;
+    }
+
+    public CodingStatsManager getCodingStatsManager() {
+        return codingStatsManager;
+    }
+
+    public void setCodingStatsManager(CodingStatsManager codingStatsManager) {
+        this.codingStatsManager = codingStatsManager;
+    }
 }
---
+++ src/main/java/xyz/thaddev/projectapis/statssystem/LeetcodeResponse.java
@@ -0,0 +1,41 @@
+package xyz.thaddev.projectapis.statssystem;
+
+import java.util.ArrayList;
+
+// import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
+// import com.fasterxml.jackson.annotation.JsonProperty; // version 2.11.1
+/* ObjectMapper om = new ObjectMapper();
+Root root = om.readValue(myJsonString, Root.class); */
+class AcSubmissionNum {
+    public int count;
+}
+
+class LeetcodeData {
+    public MatchedUser matchedUser;
+    public UserContestRanking userContestRanking;
+}
+
+class MatchedUser {
+    public SubmitStatsGlobal submitStatsGlobal;
+    public Profile profile;
+}
+
+class Profile {
+    public int ranking;
+}
+
+public class LeetcodeResponse {
+    public LeetcodeData leetcodeData;
+}
+
+class SubmitStatsGlobal {
+    public ArrayList<AcSubmissionNum> acSubmissionNum;
+}
+
+class UserContestRanking {
+    public int attendedContestsCount;
+    public double rating;
+    public int globalRanking;
+    public double topPercentage;
+}
+
