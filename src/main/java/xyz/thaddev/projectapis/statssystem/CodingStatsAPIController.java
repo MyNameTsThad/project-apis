236525f03da68d6bedefb62a2f2d6b202c0bb309
MyNameTsThad <pvpthadgaming@gmail.com>
2022-07-16 22:06:56 +0700

    set Access-Control-Allow-Origin to * on all endpoints try #2

--- src/main/java/xyz/thaddev/projectapis/statssystem/CodingStatsAPIController.java
+++ src/main/java/xyz/thaddev/projectapis/statssystem/CodingStatsAPIController.java
@@ -1,5 +1,6 @@
 package xyz.thaddev.projectapis.statssystem;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +14,13 @@ public class CodingStatsAPIController {
         ProjectApisApplication.instance.setCodingStatsAPIController(this);
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/api-v1/codingstats/get")
     private CodingStats getCodingStats() {
         return ProjectApisApplication.instance.getCodingStatsManager().getResponse();
     }
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/api-v1/codingstats/sethr")
     private void setHackerrankStats(@RequestBody HackerrankStats stats, @RequestParam String authPassword) {
         if (authPassword.equals(ProjectApisApplication.authPassword))
