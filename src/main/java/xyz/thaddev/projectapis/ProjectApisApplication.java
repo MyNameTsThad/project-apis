144b8b334dd760ac6b4dafd2190b614f4ff4ac88
MyNameTsThad <pvpthadgaming@gmail.com>
2022-07-16 22:02:30 +0700

    set Access-Control-Allow-Origin to * on all endpoints

--- src/main/java/xyz/thaddev/projectapis/ProjectApisApplication.java
+++ src/main/java/xyz/thaddev/projectapis/ProjectApisApplication.java
@@ -5,7 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import xyz.thaddev.projectapis.chatthreadsystem.ThreadAPIController;
 import xyz.thaddev.projectapis.computercontrolsystem.CommandAPIController;
 import xyz.thaddev.projectapis.computercontrolsystem.StatusResponseManager;
@@ -58,6 +61,16 @@ public class ProjectApisApplication {
                 if (times == 10) times = 0;
             }
         }, 1000, 1000);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api-v1-javaconfig").allowedOrigins("*");
+            }
+        };
     }
 
     public TimerAPIController getTimerAPIController() {
