05ea2b93028defd017ed2a16825b0ec7826622f2
MyNameTsThad <pvpthadgaming@gmail.com>
2023-01-19 17:03:56 +0700

    fix some other weird stuff

--- src/main/java/com/thaddev/projectapis/postviewscounter/PostViewsAPIController.java
+++ src/main/java/com/thaddev/projectapis/postviewscounter/PostViewsAPIController.java
@@ -1,19 +1,33 @@
 package com.thaddev.projectapis.postviewscounter;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.thaddev.projectapis.ProjectApisApplication;
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class PostViewsAPIController {
     public PostViewsAPIController() {
         ProjectApisApplication.instance.setPostViewsAPIController(this);
+        try {
+            readFromFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 
     @CrossOrigin(origins = "*")
     @GetMapping("/api-v1/postviews/getall")
@@ -31,5 +45,59 @@ public class PostViewsAPIController {
     @PostMapping("/api-v1/postviews/increment")
     private void incrementPostViews(@RequestParam int strapiID) {
         ProjectApisApplication.instance.getPostViewsCounter().incrementPostViews(strapiID);
+        try {
+            saveToFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setAllPostViews(Map<Integer, Integer> postViews) {
+        ProjectApisApplication.instance.getPostViewsCounter().setAllPostViews(postViews);
+        try {
+            saveToFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveToFile() throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(getAllPostViews());
+        File path = new File("/projectapis/db/PostViews-current.json");
+        FileOutputStream file = FileUtils.openOutputStream(path);
+        try {
+            file.write(json.getBytes());
+            ProjectApisApplication.instance.logger.info("Saved Post Views to: " + "/projectapis/db/PostViews-current.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void readFromFile() throws IOException {
+        try {
+            File path = new File("/projectapis/db/PostViews-current.json");
+            String json = FileUtils.readFileToString(path);
+
+            HashMap<Integer, Integer> obtained = new Gson().fromJson(json, new TypeToken<HashMap<Integer, Integer>>() {
+            }.getType());
+            setAllPostViews(obtained);
+
+            ProjectApisApplication.instance.logger.info("Loaded Post Views from: " + "/projectapis/db/PostViews-current.json");
+        } catch (IOException e) {
+            if (e.getMessage().equals("File '/projectapis/db/PostViews-current.json' does not exist")) {
+                ProjectApisApplication.instance.logger.warn("Storage Files not found; Creating empty files.");
+                saveToFile();
+                ProjectApisApplication.instance.logger.info("Successfully Created Storage files.");
+            }
+        }
+
     }
 }
--- src/main/java/com/thaddev/projectapis/postviewscounter/PostViewsCounter.java
+++ src/main/java/com/thaddev/projectapis/postviewscounter/PostViewsCounter.java
@@ -1,14 +1,7 @@
 package com.thaddev.projectapis.postviewscounter;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import com.thaddev.projectapis.ProjectApisApplication;
-import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,11 +10,6 @@ public class PostViewsCounter {
 
     public PostViewsCounter() {
         ProjectApisApplication.instance.setPostViewsCounter(this);
-        try {
-            readFromFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public Map<Integer, Integer> getAllPostViews() {
@@ -32,54 +20,15 @@ public class PostViewsCounter {
         return postViews.getOrDefault(strapiID, 0);
     }
 
-    private void setPostViews(int strapiID, int views) {
+    public void setPostViews(int strapiID, int views) {
         postViews.put(strapiID, views);
-        try {
-            saveToFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    }
+
+    public void setAllPostViews(Map<Integer, Integer> postViews) {
+        this.postViews = new HashMap<>(postViews);
     }
 
     public void incrementPostViews(int strapiID) {
         setPostViews(strapiID, postViews.getOrDefault(strapiID, 0) + 1);
-    }
-
-    public void saveToFile() throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(postViews);
-        File path = new File("/projectapis/db/PostViews-current.json");
-        FileOutputStream file = FileUtils.openOutputStream(path);
-        try {
-            file.write(json.getBytes());
-            ProjectApisApplication.instance.logger.info("Saved Post Views to: " + "/projectapis/db/PostViews-current.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                file.flush();
-                file.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void readFromFile() throws IOException {
-        try {
-            File path = new File("/projectapis/db/PostViews-current.json");
-            String json = FileUtils.readFileToString(path);
-
-            postViews = new Gson().fromJson(json, new TypeToken<HashMap<Integer, Integer>>() {
-            }.getType());
-            ProjectApisApplication.instance.logger.info("Loaded Post Views from: " + "/projectapis/db/PostViews-current.json");
-        } catch (IOException e) {
-            if (e.getMessage().equals("File '/projectapis/db/PostViews-current.json' does not exist")) {
-                ProjectApisApplication.instance.logger.warn("Storage Files not found; Creating empty files.");
-                saveToFile();
-                ProjectApisApplication.instance.logger.info("Successfully Created Storage files.");
-            }
-        }
-
     }
 }
